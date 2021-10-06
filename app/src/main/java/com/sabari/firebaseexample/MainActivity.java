9427430154eb60bc2599999381433a9b21d1fb67
Sabarinathan07 <64265337+Sabarinathan07@users.noreply.github.com>
2021-10-06 13:39:41 +0530

    'GetData-from-Firebase'

--- app/src/main/java/com/sabari/firebaseexample/MainActivity.java
+++ app/src/main/java/com/sabari/firebaseexample/MainActivity.java
@@ -1,22 +1,30 @@
 package com.sabari.firebaseexample;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText name;
     Button send;
+    TextView textView;
 
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference reference = database.getReference().child("Users");
+    DatabaseReference reference2 = database.getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +33,26 @@ public class MainActivity extends AppCompatActivity {
 
         name = findViewById(R.id.editTextTextPersonName);
         send = findViewById(R.id.button);
+        textView = findViewById(R.id.textView2);
+
+        reference2.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String realName = (String) dataSnapshot.child("Users").child("Name").getValue();
+                textView.setText(realName);
+            }
+
+            @Override
+            public void onCancelled(@NonNull  DatabaseError databaseError) {
+                Toast.makeText(getApplicationContext(),"Error on recieving",Toast.LENGTH_SHORT).show();
+            }
+        });
 
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String username = name.getText().toString();
-                reference.child("username").setValue(username);
+                reference.child("Name").setValue(username);
             }
         });
     }
